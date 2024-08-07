d15163d7af622958d8e498f40066e07c03944b6b
NoeliaAlmaraz <noelialmaraz09@gmail.com>
2024-08-07 14:26:23 +0200

    Falta Readme3

--- src/components/Context/Context.jsx
+++ src/components/Context/Context.jsx
@@ -11,12 +11,13 @@ const Context = createContext();
 const useAppContext = () => useContext(Context);
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId
+
+ apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
